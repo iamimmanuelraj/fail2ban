9fc163e976255265908d6ec2feebda38c83252da
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 18:00:24 +0530

    Stop nginx configs setup for now

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ apt install fail2ban
 wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/jail.conf > /etc/fail2ban/jail.local
 wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
 wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cloudflare-apiv4.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
+# wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
 read -sp "Enter your Abuseipdb Api-Key : " abipdbkey
 read -sp "Enter your Cloudflare Global Api-Key : " cfkey
 sed -i "s/$/$abipdbkey/" /etc/fail2ban/action.d/abuseipdb.conf
