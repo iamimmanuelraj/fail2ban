2699e87fc1d60cf0bdfede65f7563aa0af3e67fd
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 16:51:37 +0530

    Remote: Fix branch that is used

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 apt install fail2ban
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/master/config/jail.conf > /etc/fail2ban/jail.local
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/master/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/master/config/cloudflare-api.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/master/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
+wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/jail.conf > /etc/fail2ban/jail.local
+wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
+wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cloudflare-api.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
+wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
 systemctl start fail2ban
 systemctl enable fail2ban
 systemctl restart fail2ban
