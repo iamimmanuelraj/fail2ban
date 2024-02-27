a1ec8a626093e331d76d486a54808ebd3ef24769
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 14:58:18 +0530

    config: Ban ip in ssh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-F2B_VER_LATEST=$(curl --silent https://api.github.com/repos/fail2ban/fail2ban/releases | jq -r '.[0].tag_name')
-curl -SL https://github.com/fail2ban/fail2ban/releases/download/${F2B_VER_LATEST}/fail2ban_${F2B_VER_LATEST}-1.upstream1_all.deb -o fail2ban_${F2B_VER_LATEST}.deb
-dpkg -i fail2ban_${F2B_VER_LATEST}.deb
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+apt install fail2ban
+wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/jail.conf > /etc/fail2ban/jail.local
 systemctl start fail2ban
 systemctl enable fail2ban
 systemctl restart fail2ban
