d42c8e7950fe231038a8c399ec40c05301e621f5
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 18:25:35 +0530

    setup: Curl up the configs not wget

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 apt install fail2ban
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/jail.conf > /etc/fail2ban/jail.local
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
-wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cloudflare-apiv4.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
-# wget https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
-read -n 1 -sp "Enter your Abuseipdb Api-Key : " abipdbkey
-read -n 1 -sp "Enter your Cloudflare Global Api-Key : " cfkey
+curl https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/jail.conf > /etc/fail2ban/jail.local
+curl https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
+curl https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cloudflare-apiv4.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
+# curl https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
+read -sp "Enter your Abuseipdb Api-Key : " abipdbkey
+read -sp "Enter your Cloudflare Global Api-Key : " cfkey
 sed -i "s/$/$abipdbkey/" /etc/fail2ban/action.d/abuseipdb.conf
 sed -i "s/$/$cfkey/" /etc/fail2ban/action.d/cloudflare-apiv4.conf
 systemctl start fail2ban
