006ffd7c82fde016a1837d4860f840f3fcd2d306
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 16:50:36 +0530

    Fix naming convention issue

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 apt install fail2ban
 wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/jail.conf > /etc/fail2ban/jail.local
 wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
-wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/cloudflare-api.conf > /etc/fail2ban/action.d/cloudflare-api.conf
+wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/cloudflare-api.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
 wget https://raw.githubusercontent.com/fail2ban/fail2ban/master/config/cf-realip.conf > /etc/nginx/conf.d/cf-realip.conf
 systemctl start fail2ban
 systemctl enable fail2ban
