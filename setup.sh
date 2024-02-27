540dee98c4913fd66f8ead1a5582ddb89101ec9e
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-02-27 14:42:18 +0530

    Install script for f2b

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-sudo apt install fail2ban
+F2B_VER_LATEST=$(curl --silent https://api.github.com/repos/fail2ban/fail2ban/releases | jq -r '.[0].tag_name')
+curl -SL https://github.com/fail2ban/fail2ban/releases/download/${F2B_VER_LATEST}/fail2ban_${F2B_VER_LATEST}-1.upstream1_all.deb -o fail2ban_${F2B_VER_LATEST}.deb
+dpkg -i fail2ban_${F2B_VER_LATEST}.deb
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 systemctl start fail2ban
 systemctl enable fail2ban
 systemctl restart fail2ban
