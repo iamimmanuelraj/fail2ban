ccff3d465e38559acf3bd42492453ed85b465b0e
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-04-08 15:48:11 +0530

    setup: Setup automatic ip reporting as well
    Honor previously remove manual reporting

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ echo
 sed -i "/abuseipdb_apikey =/ s/$/$abipdbkey/" /etc/fail2ban/action.d/abuseipdb.conf
 sed -i "/cftoken =/ s/$/$cfkey/" /etc/fail2ban/action.d/cloudflare-apiv4.conf
 sed -i "/cfuser =/ s/$/$cfemail/" /etc/fail2ban/action.d/cloudflare-apiv4.conf
-chmod 640 /etc/fail2ban/action.d/abuseipdb.conf /etc/fail2ban/action.d/cloudflare-apiv4.conf
+sed -i "s/\"my-api-key\"/\"$abipdbkey\"/g" /etc/fail2ban/jail.local
+chmod 640 /etc/fail2ban/action.d/abuseipdb.conf /etc/fail2ban/action.d/cloudflare-apiv4.conf /etc/fail2ban/jail.local
 systemctl enable fail2ban
 systemctl restart fail2ban
