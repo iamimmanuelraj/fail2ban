005932b13f0d561613187a8a433404f5ff805885
Immanuel Raj <iamimmanuelraj@gmail.com>
2024-04-11 18:21:11 +0530

    Setup: Remove no-needed realip
    Not used

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ service fail2ban start
 curl -q https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/jail.local > /etc/fail2ban/jail.local
 curl -q https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/wplogin.conf > /etc/fail2ban/filter.d/wplogin.conf
 curl -q https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cloudflare-apiv4.conf > /etc/fail2ban/action.d/cloudflare-apiv4.conf
-curl -q https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/config/cf-realip.conf > /opt/easyengine/services/nginx-proxy/conf.d/cf-realip.conf
 curl -q https://raw.githubusercontent.com/iamimmanuelraj/fail2ban/main/abuseipdb-fail2ban-report.sh > /etc/fail2ban/abuseipdb-fail2ban-report.sh
 echo
 read -sp "Enter your Abuseipdb Api-Key : " abipdbkey < /dev/tty
